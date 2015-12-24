5d974f6c9af89b73c10e4c6daaa319246fb3d927
Timothy Meixell <misternu@gmail.com>
2015-12-23 22:28:02 -0700

    Add getPoss function

--- sudoku.js
+++ sudoku.js
@@ -4,3 +4,42 @@ function process(board) {
   console.log(board)
 }
 
+function solve(board, poss, empties) {
+  poss = poss || getPoss(board);
+  empties = empties || getEmpties(board);
+}
+
+
+function getPoss(board) {
+  var poss = BOARD_RANGE.map(function(x,i) { return NUMBERS.slice() })
+  for (index in BOARD_RANGE) {
+    elim(index, poss, board);
+  }
+  return poss
+}
+
+function getEmpties(board) {
+
+}
+
+function removeItemFromArray(item, array) {
+  var index = array.indexOf(item);
+  if (index != -1) {
+    array.splice(index,1)
+  }
+}
+
+function elim(index, poss, board) {
+  if (board[index] != "-") {
+    var value = board[index]
+    function elimHelper(i) {
+      removeItemFromArray(value, poss[i]);
+    }
+    ROWS[Math.floor(index/9)].forEach(elimHelper)
+    COLS[index%9].forEach(elimHelper)
+    BOXES[(Math.floor(index/27)*3)+Math.floor((index%9)/3)].forEach(elimHelper)
+    poss[index] = [];
+  }
+}
+
+console.log(getPoss(puzzle));
